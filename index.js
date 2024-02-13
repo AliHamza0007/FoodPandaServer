6a0f36ebf17a50b24ca841cac7e408c1b51a39a6
Alihamza0007 <hamzasarwer9@gmail.com>
2024-02-12 16:01:32 -0800

    update build

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 //midllerwares
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 //routing
 
 app.use("/api/v1/auth", authRoutes);
